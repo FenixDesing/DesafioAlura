5a36ee4bba811a9e651190b7bd5d31033ab94e86
JuanTic <desingjuan45@gmail.com>
2025-03-15 17:55:33 -0400

    validaciones

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+// constantes para la lista y el limite de participantes 
+
+const listaAmigos = [];
+const maxParticipantes = 10;
+
+// funcion para agregar amigos a la lista 
+
+function agregarAmigos() {
+    const input = document.getElementById("amigo");
+    const nombre = input.ariaValueMax.trim();
+
+    //validaciones 
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un numero valido.")
+        return;
+    }
+
+
+}
